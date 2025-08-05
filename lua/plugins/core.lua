f268a2f1a02b9775da7ac4d9a7efb045da399db7
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-08-05 12:02:44 -0500

    Enable html and lates parser

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -10,7 +10,7 @@ return {
         highlight = {
           enable = true,
           additional_vim_regex_highlighting = false,
-          disable = { "html", "latex" },
+          -- disable = { "html", "latex" },
         },
       })
     end,
