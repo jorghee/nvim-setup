c304f2f63c052f8eb2ce62d6e496f471bd6a290a
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-08-04 23:03:46 -0500

    Add parser plugin configuration (treesitter)

---
+++ lua/plugins/core.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "lua", "vim", "cpp", "javascript", "java", "css", "html", "markdown", "latex", "python" },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+          disable = { "html", "latex" },
+        },
+      })
+    end,
+  }
+}
