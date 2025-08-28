f93e83a26bc1a88c55fca7af125fa7e950249a92
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-08-28 11:45:57 -0500

    chore(mcp): change shutdown_delay and mcp_request_timeout time

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -4,17 +4,18 @@ return {
   build = "npm install -g mcp-hub@latest",
   config = function()
     require("mcphub").setup({
-      port = 3000,
-      config = vim.fn.expand("~/.config/nvim/mcpservers.json"),
-      on_ready = function(hub) end,
-      on_error = function(err) end,
-      shutdown_delay = 0,
-      log = {
-        level = vim.log.levels.WARN,
-        to_file = false,
-        file_path = nil,
-        prefix = "MCPHub",
-      },
+      shutdown_delay = 5 * 60 * 1000, -- 5 minutes
+      mcp_request_timeout = 5 * 60 * 1000, -- 5 minutes
+      json_decode = function(s) return require("json5").parse(s) end,
+
+      on_ready = function(hub)
+          -- Called when hub is ready
+      end,
+      on_error = function(err)
+          -- Called on errors
+      end,
+
+      global_env = {},
     })
   end,
 }
